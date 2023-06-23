bceb80f17d31c5ecef98c7ce0e87d7758ace8180
Karla C. Ferreira <karlinharural@gmail.com>
2023-06-23 09:11:18 -0300

    Update trabalho.cpp

--- trabalho.cpp
+++ trabalho.cpp
@@ -151,6 +151,32 @@ void inverterColuna(int coluna, int linha){
     }
 }
 
+//6 processando
+void nome(char **matrizNome, int coluna, int linha){
+    int a =1, int b =2, int c=3, int d=4, int e=5, int f=6, int g=7;
+    int h =8, int i =9, int j=10, int k=11, int l=12, int m=13, int n=14;
+    int o =15, int p =16, int q=17, int r=18, int s=19, int t=20, int u=21;
+    int v =22, int w =23, int x=24, int y=25, int z=26;
+    
+    //i = vetorZ[10];// letra 'k' n 11, mas como vetor comca de 0 a posicao eh 10
+   // j = vetorZ[0];// letra 'a' n 1, mas como o vetor comeca de 0 a posicao eh 0
+    
+    matrizNome[10][0] = 'K';
+    matrizNome[12][0] = 'a';
+    matrizNome[14][0] = 'r';
+    matrizNome[16][0] = 'l';
+    matrizNome[18][0] = 'a';
+}
+
+void imprimeMatrizNome(void *nome(char **matrizNome, int coluna, int linha), int coluna, int linha){
+    for(int i=0;i < linha; i++){
+        for(int j = 0;j < coluna; j++){
+            cout << matrizNome[i][j]<<"-";
+        }
+        cout<<"\n";
+    }
+}
+
 int main(){
     int linha=100;
     int coluna=100;
@@ -170,6 +196,8 @@ int main(){
         maior2 = diagonal(coluna, linha);
     }
     cout<<maior2;*/
-    inverterColuna(coluna, linha);
+    //inverterColuna(coluna, linha);
+    char **matrizNome = new char*[100];
+    imprimeMatriz(matrizNome, coluna, linha);
     return 0;
 }
